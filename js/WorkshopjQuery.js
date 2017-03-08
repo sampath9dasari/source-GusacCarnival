b3bc7dc3d91bd8d0215181d9fcc13f3240f9f428
SurenderHarsha <battusurender96@gmail.com>
2017-03-08 22:24:23 +0530

    New Ones.

--- js/WorkshopjQuery.js
+++ js/WorkshopjQuery.js
@@ -5,15 +5,46 @@ $(document).ready(function() {
     alert('edskjcxnm');
   });*/
   /*$('.view').leanModal();*/
+  $('.parallax').parallax();
+  
+ 
+var btop=50;
+var bleft=200;
+var dist=50;
+var dist2=90;
+var den=4;
+var width=$( window ).width();
+console.log(width);
+if( width < 500)
+	{
+		dist=50;
+		dist2=90;
+		den=4;
+		btop=5;
+		bleft=40;
+
+	}
+	else
+	{
+		dist=100;
+		dist2=180;
+		den=4;
+		btop=50;
+		bleft=200;
+	}
+
+$("#back").animate({ top: btop+'px' });
+$("#back").animate({ left: bleft+'px' });
 
 
 $('#menu_icon').WCircleMenu({
-width: '100px',
-height: '100px',
+
+width: dist+'px',
+height: dist+'px',
 angle_start : 0,
 delay: 80,
-distance: 180,
-angle_interval: Math.PI/8,
+distance: dist2,
+angle_interval: Math.PI/den,
 easingFuncShow:"easeOutBack",
 easingFuncHide:"easeInBack",
 step:15,
